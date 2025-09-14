e781d524aae5f02a56e42835f5841ec2b9d6e7cb
s-aakash2026 <saakash2026@gmail.com>
2025-09-14 16:31:43 +0530

    full comp

---
+++ main.js
@@ -0,0 +1,87 @@
+/*const product ={
+    item: 'pen' ,
+    price:270,
+    discount: '5%' ,
+    avalability: true,
+
+
+}
+const profile = {
+    name: "sharadha khapra",
+    followers:"569K",
+    following:4 ,
+    haveBlueTick:true,
+    work: "entrepreneur",
+    cute:"super",
+}
+marks = 70;
+if (marks > 80){
+console.log("sachin")
+}
+else if(marks <= 25){
+    console.log("billa")
+}
+else {
+console.log("aakash")
+}*/
+
+// for (let x=1;x<=100 ; x++){
+//     if(x%2 ===0)
+//     console.log(x)
+// }
+
+// game code :
+// let GameNum=50;
+// let user_num = prompt("enter the correct num");
+// while (GameNum != user_num) {
+//     user_num = prompt("enter again baby wrong!");
+// }
+// if(user_num = 50){
+//     alert("you win")
+// }
+// let full_name = prompt("enter full name");
+// let lt = full_name.length;
+// alert(`@f${full_name}${lt}`);
+// let marks = [85,97,44,37,76,60];
+// let sum=0;
+// for (let val of marks){
+//    sum += val;
+// }
+// let avg = sum / marks.length; 
+// alert(`avrage marks ${avg}`);
+
+// let comp = ["bloomberg","microsoft","uber","google","ibm","netflix"];
+// comp.splice(2,1,"ola");
+// comp.push("amazon")
+// console.log(comp);
+
+// function vovel(str) {
+//  let count=0;
+//  for(const char of str ) {
+//     if (char === 'a' || char ==='e' || char ==='i' ||char ==='o' ||char ==='u' ){
+//         count++
+//     }
+//  }
+// console.log(count);
+// }
+// nums = [2,3,4,5,6,7,8,9,10];
+// nums.forEach((num) => {
+//     console.log(num*num)
+// });
+// marks = [34,58,94,98,65];
+// let newm = marks.filter((val) => {
+//     return val > 90; 
+    
+// } );
+// console.log(newm);
+// let n = prompt("enter num");
+// let ar = [];
+// for(let i=1;i<=n;i++){
+//     ar[i-1] = i;
+// };
+// console.log(ar)
+// let sum = ar.reduce((prev,curr) =>{
+//  return prev*curr ;
+// } )
+// console.log(sum);
+
